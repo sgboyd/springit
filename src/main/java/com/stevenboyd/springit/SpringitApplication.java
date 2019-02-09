a6d9436accd44842d022713a69af4f05b16d7356
sgboyd <stevenboyd1988@gmail.com>
2019-02-09 20:10:33 +0000

    Updated

--- src/main/java/com/stevenboyd/springit/SpringitApplication.java
+++ src/main/java/com/stevenboyd/springit/SpringitApplication.java
@@ -1,13 +1,25 @@
 package com.stevenboyd.springit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			System.out.println("This is something we would only do in dev");
+		};
 	}
 
 }
---
+++ src/main/java/com/stevenboyd/springit/SpringitProperties.java
@@ -0,0 +1,20 @@
+package com.stevenboyd.springit;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+public class SpringitProperties {
+
+    /**
+     * This is our welcome message
+     */
+    private String welcomeMsg = "Hello world!";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+}
